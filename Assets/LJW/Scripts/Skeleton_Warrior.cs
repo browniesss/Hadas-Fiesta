7a6a273270735e2a0aba66986085c8370bd64617
browniess <abcdkl45678@gmail.com>
2022-05-08 19:14:46 +0900

    Enemy Update

--- Assets/LJW/Scripts/Skeleton_Warrior.cs
+++ Assets/LJW/Scripts/Skeleton_Warrior.cs
@@ -20,6 +20,7 @@ public class Skeleton_Warrior : Enemy
                 Enemy_Trace();
                 break;
             case 3:
+                Enemy_Attack();
                 break;
             case 4:
                 Enemy_Return();
@@ -34,6 +35,24 @@ public class Skeleton_Warrior : Enemy
 
     protected override void Enemy_Attack()
     {
-        throw new System.NotImplementedException();
+        if(Mana >= need_Mana)
+        {
+            switch (next_Skill)
+            {
+                // 스킬에 따라 진행
+            }
+        }
+        else // 기본 공격
+        {
+            if(Vector3.Distance(transform.position, cur_Target.transform.position) <= Attack_Range) // 사정 거리 내에 있다면 
+            {
+                anim.SetBool("isWalk", false);
+                anim.SetTrigger("isAttack");
+            }
+            else // 사정 거리 외에 있다면
+            {
+                cur_State = 2; // 추적 state로 변경
+            }
+        }
     }
 }
