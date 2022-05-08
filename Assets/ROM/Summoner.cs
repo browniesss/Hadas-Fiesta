0e1eb03602f5f0f3d4e3882c3efa1ee2f6a204ba
LSR <tofha118@naver.com>
2022-05-08 21:20:20 +0900

    r

--- Assets/ROM/Summoner.cs
+++ Assets/ROM/Summoner.cs
@@ -21,6 +21,7 @@ public class Summoner : Enemy
                 Enemy_Trace();
                 break;
             case 3:
+                Enemy_Attack();
                 break;
             case 4:
                 Enemy_Return();
@@ -30,7 +31,29 @@ public class Summoner : Enemy
 
     protected override void Enemy_Attack()
     {
-        throw new System.NotImplementedException();
+        if (Mana >= need_Mana)
+        {
+            switch (next_Skill)
+            {
+                case 1: // 1번 스킬
+                    break;
+                case 2: // 2번 스킬
+                    break;
+                    // 스킬에 따라 진행
+            }
+        }
+        else // 기본 공격
+        {
+            if (Vector3.Distance(transform.position, cur_Target.transform.position) <= Attack_Range) // 사정 거리 내에 있다면 
+            {
+                anim.SetBool("isWalk", false);
+                anim.SetTrigger("isAttack");
+            }
+            else // 사정 거리 외에 있다면
+            {
+                cur_State = 2; // 추적 state로 변경
+            }
+        }
     }
 
     void Update()
