7a46edb6c9cb6aa66e9ef03558697fb74e53f169
chaesangmin <moowol4@gmail.com>
2022-05-08 19:46:45 +0900

    Ang3

---
+++ Assets/CHAE/Scripts/AttackComponent.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackComponent : MonoBehaviour
+{
+
+    [SerializeField]
+    private int AttackAnimationNum;
+
+
+    [SerializeField]
+    public Collider[] colliders;    
+
+    public bool B_AttackOn;
+
+    
+
+    public CAnimationComponent animator;
+
+    void Start()
+    {
+        animator = (CAnimationComponent)ComponentManager.GetI.GetMyComponent(EnumTypes.eComponentTypes.AnimatorCom);
+
+        colliders = GetComponentsInChildren<Collider>();
+
+        foreach (Collider coll in colliders)
+        {
+            coll.enabled = false;
+            Debug.Log(coll.name);
+            Debug.Log(coll.gameObject.name);
+        }
+
+        foreach (var item in animator.Attackclips)
+        {
+            Debug.Log(item.name);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (!B_AttackOn)
+            {
+                B_AttackOn = true;
+                Debug.Log("공격");
+                AttackOn();
+
+                foreach (Collider coll in colliders)
+                {
+                    coll.enabled = true;
+                }
+            }
+        }
+    }
+    public void AttackOn()
+    {
+        
+        if (animator == null)
+        {
+            Debug.Log("이거 실행");
+            animator = (CAnimationComponent)ComponentManager.GetI.GetMyComponent(EnumTypes.eComponentTypes.AnimatorCom);
+        }
+       
+
+        animator.SetBool(EnumTypes.eAnimationState.Attack, true);
+    }    
+        
+    public void AttackEnd(int num)
+    {
+
+        Debug.Log("공격 끝");
+        animator.SetBool(EnumTypes.eAnimationState.Idle, true);
+
+
+        foreach (Collider coll in colliders)
+        {
+            coll.enabled = false;
+        }
+
+        B_AttackOn = false;
+    }
+
+   
+}
