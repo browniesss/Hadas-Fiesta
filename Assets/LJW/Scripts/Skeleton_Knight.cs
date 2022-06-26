a9d30f5fab0f26320853abcc8185becdc56fb716
1 <1>
2022-06-26 20:32:39 +0900

    Skeleton_Knight 수정

--- Assets/LJW/Scripts/Skeleton_Knight.cs
+++ Assets/LJW/Scripts/Skeleton_Knight.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Skeleton_Knight : Enemy
 {
+    [SerializeField]
+    private GameObject spawn_Skeleton_Prefab; // 소환할 스켈레톤 몬스터 프리팹
+
     // 방패를 들고 이동하고 방패를 들어올린 쪽에서 입는 데미지는 감소하기 때문에 콜라이더를 하나 더 만들어줘야함.
     void Start()
     {
@@ -16,6 +19,9 @@ public class Skeleton_Knight : Enemy
     void Update()
     {
         Enemy_FSM();
+
+        if (Input.GetKeyDown(KeyCode.F1))
+            Skeleton_Knight_Skill_3();
     }
 
     protected override void Enemy_Attack()
@@ -29,9 +35,6 @@ public class Skeleton_Knight : Enemy
                     break;
                 case 2: // 2번 스킬
                     Skeleton_Knight_Skill_2();
-                    break;
-                case 3:
-                    Skeleton_Knight_Skill_3();
                     break;
                     // 스킬에 따라 진행
             }
@@ -89,9 +92,21 @@ public class Skeleton_Knight : Enemy
         // 전방을 방패로 힘껏 밀어 올려치는 애니메이션
     }
 
-    void Skeleton_Knight_Skill_3() // 스켈레톤 나이트 3번스킬 
+    void Skeleton_Knight_Skill_3() // 스켈레톤 나이트 3번스킬 ( 패시브 스킬 : 체력 50 % 미만이 되면 발동 ) 
     {
         Debug.Log("포효 발동");
         // 칼로 방패를 2번 친 후 양팔을 벌린 채 포효를 내지르는 애니메이션.
+
+        // 몬스터 8마리 소환
+        // 소환되는 몬스터의 땅속에서 올라오는 애니메이션 실행
+        for (int i = 0; i < 8; i++)
+        {
+            GameObject spawned_enemy = GameObject.Instantiate(spawn_Skeleton_Prefab);
+            spawned_enemy.gameObject.name = i.ToString() + "번째";
+
+            Vector3 v = new Vector3(Mathf.Sin(30.0f * i) * 5.0f, 0.0f, Mathf.Cos(30.0f * i) * 5.0f);
+
+            spawned_enemy.transform.position = transform.position + v;
+        }
     }
 }
