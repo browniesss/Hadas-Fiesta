063b0c510df5d833e3d3fd5d34bce7155dd64a5a
JO <kio7147@naver.com>
2022-06-27 21:48:15 +0900

    1234

--- Assets/JO/Scripts/MySingleton.cs
+++ Assets/JO/Scripts/MySingleton.cs
@@ -2,18 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Singleton<T> : MonoBehaviour where T :MonoBehaviour
+public class MySingleton<T> : MonoBehaviour where T : MonoBehaviour
 {
     protected static T instance;
     public static T Instance
     {
         get
         {
-            if(instance==null)
+            if (instance == null)
             {
                 instance = (T)FindObjectOfType(typeof(T));
             }
-            if(instance)
+            if (instance)
             {
                 return instance;
             }
