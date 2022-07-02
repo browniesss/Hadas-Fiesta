17167c33e173f02100fa3fe1dc5211bf1ae562c5
l <ll>
2022-07-02 17:58:44 +0900

    d

---
+++ Assets/ROM/scripts/GameMG.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMG : Singleton<GameMG>
+{
+
+    void startGame()
+    {
+        // 게임 저장데이터
+        //게임필드
+        //캐릭터 생성
+        // UI매니저 호출
+    }
+
+    //로딩 씬 만들기
+    //로딩중 
+
+    //게임종료 저장
+
+    //몬스터 캐릭터 로드
+    
+    //몬스터랑 캐릭터 만드는 규칙
+    //스탯종류가 어떤지 역할 
+    //캐릭터 
+    //스탯 클래스 
+    //스킬 데이터 계산
+
+
+    void Start()
+    {
+        
+    }
+
+}
---
+++ Assets/ROM/scripts/LoadMG.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//캐릭터  스탯증가.
+//스킬 
+//액션 -
+
+
+public class LoadMG : MonoBehaviour
+{
+ 
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
