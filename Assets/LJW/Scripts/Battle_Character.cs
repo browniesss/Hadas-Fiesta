42843b2f25435789cfc8fd7aa80fcce22c7e56b6
1 <1>
2022-07-02 17:56:34 +0900

    fsm, state handler 주석

--- Assets/LJW/Scripts/Battle_Character.cs
+++ Assets/LJW/Scripts/Battle_Character.cs
@@ -2,11 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+ex ) 원거리, 근거리 처럼 다른 스테이트 프로세스를 수행해야 한다면 
+스테이트 처리기를 상속받은 클래스를 구현해서, 예를들어서 patrol 이 필요없는 ai 종류의 스테이트 처리기, 필요한 종료의 스테이트 처리기 등을
+구현해야함 . 그리고 state_handler 를 상속이 아니라 ai 처럼 가지고 있을 수 있게 해야함.
+ */
+
 public class Battle_Character : State_Handler
 {
     [SerializeField]
     protected FSM_AI ai = new FSM_AI();
-
+    
+    // 스테이트 처리기를 생성. 캐릭터에 따라서 다른 스테이트 처리기를 받아옴. 
     public GameObject cur_Target;
     [SerializeField]
     protected Vector3 return_Pos; // 복귀할 위치
@@ -45,14 +52,7 @@ public class Battle_Character : State_Handler
 
     protected override void Trace_Process()
     {
-        //if (Vector3.Distance(transform.position, cur_Target.transform.position) <= Attack_Range) // 타겟에 닿았다면
-        //{
-        //    cur_State = 3; // 공격 상태로 변경
-        //}
-        //else
-        //{
         Destination_Move(cur_Target.transform.position);
-        //}
     }
 
     protected override void Attack_Process()
