457217869f6742d797049113f82957c81499bf8a
JO <kio7147@naver.com>
2022-07-09 16:34:35 +0900

    공격 모션 수정, 방어시스템 완성

--- Assets/Scripts/Player/CGuardComponent.cs
+++ Assets/Scripts/Player/CGuardComponent.cs
@@ -4,29 +4,79 @@ using UnityEngine;
 
 public class CGuardComponent : BaseComponent
 {
-    [Header("Guard Options")]
+    public CMoveComponent movecom;
+
+    [Header("============Guard Options============")]
     public float GuardTime;//최대로 가드를 할 수 있는 시간
     public int MaxGuardGauge;//
 
-    [Header("Cur Values")]
+    [Header("============Cur Values============")]
     public int CurGuardGauge;
     public bool nowGuard;
     public float GaugeDownInterval;
+    //public bool playingCor;
+    public IEnumerator coroutine;
+    public delegate void Invoker();
 
-
-        
     public void Guard()
     {
+        if (movecom == null)
+            movecom = ComponentManager.GetI.GetMyComponent(EnumTypes.eComponentTypes.MoveCom) as CMoveComponent;
 
+        if (movecom.curval.IsGuard)
+            return;
+
+        movecom.curval.IsGuard = true;
+
+        if (coroutine != null)
+        {
+            //playingCor = false;
+            //Debug.Log("실행중 나감");
+            StopCoroutine("Cor_TimeCounter");
+            coroutine = null;
+
+        }
+        coroutine = Cor_TimeCounter(GuardTime, GuardDown);
+        StartCoroutine(coroutine);
     }
 
     //
-    public void GaugeDown()
+    public void GuardDown()
     {
+        if (!movecom.curval.IsGuard)
+            return;
 
+        if (coroutine!=null)
+        {
+            //playingCor = false;
+            //Debug.Log("실행중 나감");
+            StopCoroutine(coroutine);
+            coroutine = null;
+        }
+            
+
+        movecom.curval.IsGuard = false;
     }
 
-
+    //공격이 시작된지 일정 시간 뒤에 이펙트를 실행해야 할 때 사용
+    IEnumerator Cor_TimeCounter(float time, Invoker invoker)
+    {
+        //playingCor = true;
+        float starttime = Time.time;
+        //Debug.Log("다시시작");
+        while (true)
+        {
+            if ((Time.time - starttime) >= time)
+            {
+                //Debug.Log("시간다됨");
+                invoker.Invoke();
+                coroutine = null;
+                //playingCor = false;
+                yield break;
+            }
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
+    }
 
 
     public override void InitComtype()
