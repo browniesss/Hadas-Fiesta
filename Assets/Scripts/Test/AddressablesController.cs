02e6343001c97f22ed097cd03cfb6e8adf7b7664
[Github <[Github>
2022-07-15 18:26:09 +0900

    ㅅㄹ

--- Assets/Scripts/Test/AddressablesController.cs
+++ Assets/Scripts/Test/AddressablesController.cs
@@ -1,26 +1,47 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+
 
 public class AddressablesController : MonoBehaviour
 {
 	[SerializeField]
 	private string _label;
-	private Transform _parent;
+	bool flag = true;
+	[SerializeField]
 	private List<GameObject> _createdObjs { get; } = new List<GameObject>();
 
 	private void Start()
 	{
-		//_parent = GameObject.Find("SampleScene").transform;
-		Instantiate();
+
+		//Instantiate("test1");
+		Instantiate("Monster");
+
+		temp_Show_list();
 	}
 
-	private async void Instantiate()
+	private async void Instantiate(string label)
 	{
-		Debug.Log("생성s");
+		
+		await AddressablesLoader.InitAssets_label(label, _createdObjs);
+		//setPos();
 
-		await AddressablesLoader.InitAssets(_label, _createdObjs, _parent);
-		Debug.Log("생성");
-		setPos();
+	
+		//temp_Show_list();
+
+	}
+
+	void temp_Show_list()
+	{
+		Debug.Log("이름보기");
+		int c=0;
+
+		foreach (var obj in _createdObjs)
+		{
+			c++;
+			Debug.Log("생성네임"+obj.name);
+		}
+		Debug.Log(c);
 
 	}
 
@@ -29,9 +50,58 @@ public class AddressablesController : MonoBehaviour
 		foreach (var obj in _createdObjs)
 		{
 			obj.transform.position = new Vector3(0, 0, 0);
-
+		
 		}
 
+	}
+
+	//이름으로 썼으면 addAsset해주기
+	public async void addAsset(string name)
+	{
+		await AddressablesLoader.InitAssets_name(name, _createdObjs);
+
+	}
+
+    private void Update()
+    {
+		//if(_createdObjs!=null)
+  //      {
+		//	foreach (var obj in _createdObjs)
+		//	{
+		//		Debug.Log("생성네임" + obj.name);
+		//	}
+		//}
+		
+	}
+
+    void add()
+	{
+		foreach (var obj in AddressablesLoader.tempobj)
+		{
+			Instantiate(obj, new Vector3(0, 0, 0), Quaternion.identity);
+		}
+		AddressablesLoader.tempobj.Clear();
+		flag = false;
+	}
+
+	public void tempdes()
+	{
+		foreach (var obj in _createdObjs)
+		{
+			Destroy_Obj(obj);
+		}
+
+	}
+
+
+	public void Destroy_Obj(GameObject obj)  //삭제할 오브젝트
+	{
+		if (!Addressables.ReleaseInstance(obj))
+		{
+			Addressables.ReleaseInstance(obj);
+			Destroy(obj);
+			_createdObjs.Remove(obj);
+		}
 	}
 
 }
--- Assets/Scripts/Test/AddressablesLoader.cs
+++ Assets/Scripts/Test/AddressablesLoader.cs
@@ -1,24 +1,55 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine;
-//using UnityEngine.AddressableAssets;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
 public static class AddressablesLoader
 {
-	public static async Task InitAssets<T>(string label, List<T> createdObjs, Transform parent)
+	public static List<GameObject> tempobj = new List<GameObject>();
+
+	//饭捞喉肺 积己
+	//Addressables.Release();
+	public static async Task InitAssets_label<T>(string label, List<T> createdObjs)
 		where T : Object
 	{
 		Debug.Log("积己傈" + label);
 
 
-		//var locations = await Addressables.LoadResourceLocationsAsync(label).Task;
+		var locations = await Addressables.LoadResourceLocationsAsync(label).Task;
 		Debug.Log("积己啊び廉咳" + label);
 
 
-	//	foreach (var location in locations)
+		foreach (var location in locations)
 		{
-		//	createdObjs.Add(await Addressables.InstantiateAsync(location).Task as T);
+			createdObjs.Add(await Addressables.InstantiateAsync(location).Task as T);
 			Debug.Log("积己" + label);
 		}
 	}
+
+	//捞抚栏肺 积己
+	//Addressables.ReleaseInstance();
+	public static async Task InitAssets_name<T>(string object_name, List<T> createdObjs)
+		where T : Object
+	{
+		//AsyncOperationHandle<GameObject> operationHandle=
+		// Addressables.LoadAssetAsync<GameObject>(object_name);
+
+		Addressables.LoadAssetAsync<GameObject>(object_name).Completed += ObjectLoadDone;
+
+		// yield return operationHandle;
+
+		//createdObjs.Add(operationHandle.Result as T);
+
+	}
+
+	private static void ObjectLoadDone(AsyncOperationHandle<GameObject> obj)
+	{
+		GameObject gameObject = obj.Result;
+		tempobj.Add(gameObject);
+
+		Debug.Log("积己");
+
+	}
+
 }
