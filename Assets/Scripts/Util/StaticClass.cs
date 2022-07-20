4c0df63ba578c530e4a278e67c291e603706a4f4
chaesangmin <moowol4@gmail.com>
2022-07-21 07:55:56 +0900

    ttq

--- Assets/Scripts/Util/StaticClass.cs
+++ Assets/Scripts/Util/StaticClass.cs
@@ -8,4 +8,6 @@ public class StaticClass
     public static string ADD = "¾Ó";
     public static string ADD2 = "Damege_Absorption";
     public static int MonsterCount = 8;
+    public static string AttackEnd = "AttackEnd";
+    public static string AttackMove = "AttackMove";
 }
